0a32bc687b2bcb06facac25d2509db7b7fd2dbcb
Danilo Alexandre <daniloalexandre@sti.ufpb.br>
2020-10-15 22:20:25 -0300

    ajuste da validação do campo open_on_weekend

--- src/controllers/OrphanageController.ts
+++ src/controllers/OrphanageController.ts
@@ -54,7 +54,7 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekend,
+            open_on_weekend : open_on_weekend === 'true',
             images
         }
 
